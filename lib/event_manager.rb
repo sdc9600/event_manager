17beb5c0b3c3c83a1bd9503d91f6dc4faba6c0f3
sdc9600 <149299980+sdc9600@users.noreply.github.com>
2024-01-01 00:54:36 +1030

    Additional functions added

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,7 +2,30 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 require 'time'
+require 'date'
 
+def most_frequent_date_registered(date_stamp, dates_registered)
+  if date_stamp[6] == ' '
+    date_stamp.slice!(6..7)
+  end
+  date_stamp = date_stamp.insert(-3, '20')
+  puts date_stamp
+  inspect_date = Date.strptime(date_stamp, '%m/%d/%Y')
+
+  if dates_registered[inspect_date.wday] == nil
+    dates_registered[inspect_date.wday] = 1
+  else
+    dates_registered[inspect_date.wday] += 1
+  end
+end
+
+def return_max_date(dates_registered)
+  i = 0
+  days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+  dates_registered.transform_keys!.with_index {|k, i| "#{days[i]}"}
+  max_value = dates_registered.values.max
+  puts dates_registered.select {|k, v| v == max_value}
+end
 def clean_phone_numbers(number)
   number.gsub!(/[\D]/, '')
   
@@ -16,15 +39,18 @@ def clean_phone_numbers(number)
   end
 end
 
-def hour_registered(time)
-  t = Time.new
-  puts time.strptime("%d/%m/%y")
-  begin
-    puts t
-    rescue
-      puts "Invalid Time!!"
-    end
+def hour_registered(time, hours_registered)
+  if hours_registered[time] == nil
+     hours_registered[time] = 1
+  else
+    hours_registered[time] += 1
   end
+end
+
+def most_frequent(hours_registered)
+  max_value = hours_registered.values.max
+  puts hours_registered.select{|k, v| v == max_value}
+end
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -65,11 +91,16 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+hours_registered = {}
+dates_registered = {}
+
 contents.each do |row|
-  
-  time_stamp = row[1]
-  hour_registered(time_stamp)
+  date_stamp = row[1][0..7]
+  most_frequent_date_registered(date_stamp, dates_registered)
+  time_stamp = row[1][-5..-1].strip[0..1]
+  hour_registered(time_stamp, hours_registered)
   number = row[5]
+
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
@@ -79,3 +110,5 @@ contents.each do |row|
   save_thank_you_letter(id,form_letter)
   puts clean_phone_numbers(number)
 end
+most_frequent(hours_registered)
+return_max_date(dates_registered)
